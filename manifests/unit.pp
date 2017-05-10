573eaea4f774931475d4a14d3ea14f1d040a1d1a
Samual Laakso <slaakso@hearst.com>
2017-05-10 17:49:42 -0400

    linting

--- manifests/unit.pp
+++ manifests/unit.pp
@@ -1,36 +1,35 @@
 # Create CoreOS unit files
-define coreos::unit (
+define coreos::unit(
 $description  = $name,
-$after        = undef,
-$requires     = undef,
-$wants        = undef,
+$after,
+$requires,
+$wants,
 $execstart    = undef,
-$execstartpre = undef,
+$execstartpre,
 $restartsec   = undef,
 $restart      = undef,
 ){
-  concat { "/etc/systemd/system/$name.service":
-    ensure  => file,
-    owner   => "0",
-    group   => "0",
-    mode    => "0644",
-    notify  => Exec[systemctl reload]
+  concat { "/etc/systemd/system/${name}.service":
+    ensure => file,
+    owner  => 0,
+    group  => 0,
+    mode   => 0644,
+    notify => Exec['systemctl reload']
   }
-  concat::fragment { "$name_unit":
-    target  => "/etc/systemd/system/$name.service",
+  concat::fragment { "${name}_unit":
+    target  => "/etc/systemd/system/${name}.service",
     content => template('coreos/unit.template.erb'),
     order   => '01'
   }
-  concat::fragment { "$name_service":
-    target  => "/etc/systemd/system/$name.service",
+  concat::fragment { "${name}_service":
+    target  => "/etc/systemd/system/${name}.service",
     content => template('coreos/service.template.erb'),
     order   => '99'
   }
 
   exec { 'systemctl reload':
     command     => '/usr/bin/systemctl daemon-reload',
-    user        => "0",
+    user        => 0,
     refreshonly => true
   }
-
 }
