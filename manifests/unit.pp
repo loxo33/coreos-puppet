03eaa5ab80f4634b2214ddb455ce29371ddd86e0
Samual Laakso <slaakso@hearst.com>
2017-05-11 10:45:34 -0400

    switch from concat to file

--- manifests/unit.pp
+++ manifests/unit.pp
@@ -9,24 +9,14 @@ $execstartpre = undef,
 $restartsec,
 $restart,
 ){
-  concat { "/etc/systemd/system/${name}.service":
-    ensure => file,
-    owner  => 0,
-    group  => 0,
-    mode   => 0644,
-    notify => Exec['systemctl reload']
-  }
-  concat::fragment { "${name}_unit":
-    target  => "/etc/systemd/system/${name}.service",
+  file {"/etc/systemd/system/${name}.service":
+    ensure  => file,
+    owner   => 0,
+    group   => 0,
+    mode    => 0644,
     content => template('coreos/unit.template.erb'),
-    order   => '01'
+    notify  => Exec['systemctl reload']
   }
-  concat::fragment { "${name}_service":
-    target  => "/etc/systemd/system/${name}.service",
-    content => template('coreos/service.template.erb'),
-    order   => '99'
-  }
-
   exec { 'systemctl reload':
     command     => '/usr/bin/systemctl daemon-reload',
     user        => 0,
