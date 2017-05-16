a7d7be3422f2f773b54e3c0f64dc59e3c271343c
Samual Laakso <slaakso@hearst.com>
2017-05-16 13:29:42 -0400

    try erb

--- manifests/unit.pp
+++ manifests/unit.pp
@@ -11,14 +11,14 @@ Optional[String] $restartsec   = undef,
 Optional[String] $restart      = undef,
 Optional[Array]  $wantedby     = undef,
 ){
-  include ::coreos
-  include ::stdlib
+#  include ::coreos
+#  include ::stdlib
   file {"/etc/systemd/system/${name}.service":
     ensure  => file,
     owner   => 0,
     group   => 0,
     mode    => '0644',
-    content => epp('coreos/unit.template.epp'),
+    content => epp('coreos/unit.template.erb'),
     notify  => Class['coreos::systemctl::daemon_reload'],
   }
 }
